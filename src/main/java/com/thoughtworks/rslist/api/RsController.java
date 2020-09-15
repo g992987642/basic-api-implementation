ae044f70fd3a63014d41a2d01397f7c66741c7d6
g992987642 <992987642@qq.com>
2020-09-15 20:28:07 +0800

    [guhao]:implement add one hot search event

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,28 +1,42 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-    private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private List<String> rsList = initRsList();
+
+    private List<String> initRsList() {
+        List<String> tempList = new ArrayList<>();
+        tempList.add("第一条事件");
+        tempList.add("第二条事件");
+        tempList.add("第三条事件");
+        return tempList;
+    }
+
 
     @GetMapping("/rs/{index}")
     public String getRsEvent(@PathVariable int index) {
-        return rsList.get(index-1);
+        return rsList.get(index - 1);
     }
+
     @GetMapping("/rs/event")
-    public String getRsEventByRange(@RequestParam int start,@RequestParam int end){
-        return rsList.subList(start-1,end).toString();
+    public String getRsEventByRange(@RequestParam int start, @RequestParam int end) {
+        return rsList.subList(start - 1, end).toString();
     }
+
     @GetMapping("/rs/list")
-    public String getAllRsEvent(){
+    public String getAllRsEvent() {
         return rsList.toString();
+    }
+
+    @PostMapping("/rs/event")
+    public void addOneRsEvent(@RequestBody String event) {
+        rsList.add(event);
     }
 
 
--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RsControllerTest {
 
     @Autowired
@@ -47,6 +49,19 @@ class RsControllerTest {
                 .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
     }
 
+    @Test
+    void should_add_one_rs_event() throws Exception {
+        mockMVC.perform(get("/rs/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
+        mockMVC.perform(post("/rs/event").content("第四条事件"))
+                .andExpect(status().isOk());
+        mockMVC.perform(get("/rs/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件, 第四条事件]"));
+
+
+    }
 
 
 
