95f4bf0e5cc60839d0e6d2de6b0d744cc5ae2bcb
g992987642 <992987642@qq.com>
2020-09-16 18:54:45 +0800

    [guhao]:add a User Object in RsEvent

--- src/main/java/com/thoughtworks/rslist/dto/RsEvent.java
+++ src/main/java/com/thoughtworks/rslist/dto/RsEvent.java
@@ -1,30 +1,21 @@
 package com.thoughtworks.rslist.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RsEvent {
     private String eventName;
     private String keyWord;
-
-    public RsEvent() {
-    }
+    private User user;
 
     public RsEvent(String eventName, String keyWord) {
         this.eventName = eventName;
         this.keyWord = keyWord;
     }
 
-    public String getEventName() {
-        return eventName;
-    }
 
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public String getKeyWord() {
-        return keyWord;
-    }
-
-    public void setKeyWord(String keyWord) {
-        this.keyWord = keyWord;
-    }
 }
